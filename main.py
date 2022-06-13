6473154e4be3e99f0a288ec9310ca7345d579b44
lovxonvy <87499021+triennale@users.noreply.github.com>
2022-06-13 21:04:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,98 @@
+import time
+import sys
+import random
+import colorama
+import os
+colorama.init(autoreset=False)
+def clr():
+    _ = os.system("cls")
+    return
+def print00025(text):
+    for c in text:
+        sys.stdout.write(colorama.Fore.CYAN + c)
+        sys.stdout.flush()
+        time.sleep(0.0025)
+    sys.stdout.write("\n")
+def print0040(text):
+    for c in text:
+        sys.stdout.write(colorama.Fore.CYAN + c)
+        sys.stdout.flush()
+        time.sleep(0.040)
+    sys.stdout.write("\n")
+def eth():
+    delay = 0.25
+    delay2 = 3
+    print00025(""" ___ ___  ____  ____   ____  ____    ____      ____   ____   ____  __ __  ______      ____    ___   __    __ 
+|   T   Tl    j|    \ l    j|    \  /    T    |    \ l    j /    T|  T  T|      T    |    \  /   \ |  T__T  T
+| _   _ | |  T |  _  Y |  T |  _  YY   __j    |  D  ) |  T Y   __j|  l  ||      |    |  _  YY     Y|  |  |  |
+|  \_/  | |  | |  |  | |  | |  |  ||  T  |    |    /  |  | |  T  ||  _  |l_j  l_j    |  |  ||  O  ||  |  |  |
+|   |   | |  | |  |  | |  | |  |  ||  l_ |    |    \  |  | |  l_ ||  |  |  |  |      |  |  ||     |l  `  '  !
+|   |   | j  l |  |  | j  l |  |  ||     |    |  .  Y j  l |     ||  |  |  |  |      |  |  |l     ! \      / 
+l___j___j|____jl__j__j|____jl__j__jl___,_j    l__j\_j|____jl___,_jl__j__j  l__j      l__j__j \___/   \_/\_/  
+                                                                                                             
+                                                           
+                                    #MADE BY akame#6222""")
+    print0040("Connecting To Api")
+    time.sleep(delay)
+    r1 = random.randint(20, 50)
+    print0040("Starting " + str(r1) + " Server(s)")
+    time.sleep(delay)
+    print0040("Server(s) Started")
+    time.sleep(delay)
+    print0040("Sending Crypto Address To Server(s)")
+    time.sleep(delay)
+    print0040("Started Overclocking On Server(s)")
+    time.sleep(delay)
+    print0040("Starting In 3 Seconds")
+    time.sleep(3)
+    choice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    while True:
+        for u in range(150):
+            clr()
+            print(colorama.Fore.RED + """ ___ ___  ____  ____   ____  ____    ____      ____   ____   ____  __ __  ______      ____    ___   __    __ 
+|   T   Tl    j|    \ l    j|    \  /    T    |    \ l    j /    T|  T  T|      T    |    \  /   \ |  T__T  T
+| _   _ | |  T |  _  Y |  T |  _  YY   __j    |  D  ) |  T Y   __j|  l  ||      |    |  _  YY     Y|  |  |  |
+|  \_/  | |  | |  |  | |  | |  |  ||  T  |    |    /  |  | |  T  ||  _  |l_j  l_j    |  |  ||  O  ||  |  |  |
+|   |   | |  | |  |  | |  | |  |  ||  l_ |    |    \  |  | |  l_ ||  |  |  |  |      |  |  ||     |l  `  '  !
+|   |   | j  l |  |  | j  l |  |  ||     |    |  .  Y j  l |     ||  |  |  |  |      |  |  |l     ! \      / 
+l___j___j|____jl__j__j|____jl__j__jl___,_j    l__j\_j|____jl___,_jl__j__j  l__j      l__j__j \___/   \_/\_/  
+                                                                                                                  
+                                                           
+                                    #MADE BY akame#6222""")
+            rc = random.choices(choice, k=64)
+            print(colorama.Fore.RED + "Generated Code - " + "".join(rc) + " - 0.00$")
+            time.sleep(0.075)
+        clr()
+        r1 = random.randint(1644, 9856)
+        rc = random.choices(choice, k=64)
+
+        print(colorama.Fore.GREEN + """ ___ ___  ____  ____   ____  ____    ____      ____   ____   ____  __ __  ______      ____    ___   __    __ 
+|   T   Tl    j|    \ l    j|    \  /    T    |    \ l    j /    T|  T  T|      T    |    \  /   \ |  T__T  T
+| _   _ | |  T |  _  Y |  T |  _  YY   __j    |  D  ) |  T Y   __j|  l  ||      |    |  _  YY     Y|  |  |  |
+|  \_/  | |  | |  |  | |  | |  |  ||  T  |    |    /  |  | |  T  ||  _  |l_j  l_j    |  |  ||  O  ||  |  |  |
+|   |   | |  | |  |  | |  | |  |  ||  l_ |    |    \  |  | |  l_ ||  |  |  |  |      |  |  ||     |l  `  '  !
+|   |   | j  l |  |  | j  l |  |  ||     |    |  .  Y j  l |     ||  |  |  |  |      |  |  |l     ! \      / 
+l___j___j|____jl__j__j|____jl__j__jl___,_j    l__j\_j|____jl___,_jl__j__j  l__j      l__j__j \___/   \_/\_/  
+                                                                                                                         
+                                    #MADE BY akame#6222""")
+        print(colorama.Fore.GREEN + "Generated Code - " + "".join(rc) + " - " + str(r1) + "$")
+        time.sleep(1)
+        print(colorama.Fore.GREEN + "Sending Money To Main Address")
+        time.sleep(1)
+        print(colorama.Fore.GREEN + "you will be more luckly next time (coded by akame#6222")
+        time.sleep(5)
+        return
+while True:
+    print(colorama.Fore.CYAN + """
+    1. Credits
+    2. Change Address
+    3. Etherium Stealer""")
+    main = input("> ")
+    if main == "3":
+        eth()
+    if main == "1":
+        print("Program Was Fully Coded By akame #6222")
+        input("")
+    if main == "2":
+        main = input("soeKNBBjbbTwoxwdYGNwGpKYKxuPOdbFdGHUhyuGeScCFXcONbBfShdJAmcZviVY")
+    
